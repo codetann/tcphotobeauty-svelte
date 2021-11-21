46688a49beeb2bf9733417f94b0bc31a6f7a2cca
Tanner <thetannerthomas@gmail.com>
2021-11-20 22:41:19 -0700

    refactor footer component

---
+++ src/components/Footer.svelte
@@ -0,0 +1,22 @@
+<p class="text-xs font-medium mx-4 text-center">
+	Salt Lake City, Utah - Husband + Wife - Wedding Photographers/Videographers
+</p>
+<div class="social-icons">
+	<div class="social-icon" />
+	<div class="social-icon" />
+	<div class="social-icon" />
+	<div class="social-icon" />
+	<div class="social-icon" />
+</div>
+
+<style lang="postcss">
+	p {
+		@apply my-14;
+	}
+	.social-icons {
+		@apply flex w-full justify-center items-center space-x-2 pb-10;
+	}
+	.social-icon {
+		@apply h-8 w-8 rounded-full bg-black cursor-pointer;
+	}
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,11 @@
 <script>
-	import '../app.postcss';
 	import Navigation from '$components/Navigation.svelte';
+	import Footer from '$components/Footer.svelte';
+	import '../app.postcss';
 </script>
 
 <Navigation />
-<main class="relative">
+<main class="relative flex flex-col w-screen max-w-full items-center">
 	<slot />
+	<Footer />
 </main>
