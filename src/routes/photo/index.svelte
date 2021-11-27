629c5ada294cbf1e6f150a61a86d176af52c6bdb
codetann <thetannerthomas@gmail.com>
2021-11-26 18:41:32 -0700

    refactor photo page

--- src/routes/photo/index.svelte
+++ src/routes/photo/index.svelte
@@ -1,69 +1,51 @@
 <script context="module">
-	// code here
-	let array = [1, 2, 3, 4, 5, 6, 7];
-	function test() {}
+	let array = [1, 2, 3, 4, 5];
+
+	let PACKAGES = [
+		{
+			title: 'Package 1',
+			price: '$1000',
+			services: ['4 Hour Coverage', 'Bridals', 'Engagements']
+		},
+		{
+			title: 'Package 2',
+			price: '$1200',
+			services: ['8 Hour Coverage', 'Bridals', 'Engagements']
+		}
+	];
 </script>
 
 <svelte:head>
 	<title>Photo | T&C</title>
 </svelte:head>
 
-<!-- UPDATE TO FLEXBOX  https://tailwindgrids.com/#/ -->
-
 <section class="w-full max-w-5xl">
-	<!-- <div
-		class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 max-w-sm md:max-w-2xl lg:max-w-4xl content-center w-full"
-	> -->
+	<!-- clients -->
 	<div class="flex flex-wrap justify-center items-center ">
 		{#each array as _}
 			<div class="h-56 flex-1 bg-red-500 m-5 min" />
 		{/each}
 	</div>
 
-	<div class="flex flex-col justify-start items-center mt-6 mx-6 ">
-		<div class="h-0.5 bg-opacity-10 bg-black w-64 sm:w-96 md:w-full max-w-2xl mb-8" />
-		<p class="text-sm mb-6">PRICING</p>
-
-		<div class="w-full flex  max-w-5xl py-20  mb-10 justify-center items-center bg-red-300">
-			<!-- <div class="h-96 flex-1 bg-blue-500" /> -->
-			<div class=" w-full">
-				<span class="flex w-full justify-center items-center mb-10 space-x-2 ">
-					<h1 class="text-3xl  text-center font-black">Package 1</h1>
-					<p class="text-sm">●</p>
-					<p class="text-3xl ">$900</p>
-				</span>
-				<ul class="text-center space-y-5 border-price py-10 max-w-3xl">
-					<li>4 hour coverage</li>
-					<li>Bridals</li>
-					<li>Engagements</li>
-				</ul>
-			</div>
-		</div>
-		<div class="w-full flex  max-w-5xl py-20  mb-10 justify-center">
-			<!-- <div class="h-96 flex-1 bg-blue-500" /> -->
-			<div class=" ">
-				<span class="flex w-full justify-center items-center mb-10 space-x-2 ">
-					<h1 class="text-3xl  text-center font-black">Package 1</h1>
-					<p class="text-sm">●</p>
-					<p class="text-3xl ">$900</p>
-				</span>
-				<ul class="text-center space-y-5 border-price py-10">
-					<li>4 hour coverage</li>
-					<li>Bridals</li>
-					<li>Engagements</li>
-				</ul>
-			</div>
-		</div>
-		<div class="w-full flex flex-wrap max-w-5xl py-20 border-price">
-			<!-- <div class="h-96 flex-1 bg-blue-500" /> -->
-			<div class="">
-				<h1 class="text-3xl underline text-center mb-5 font-black">Package 1</h1>
-				<ul class="text-center space-y-5">
-					<li>4 hour coverage</li>
-					<li>Bridals</li>
-					<li>Engagements</li>
-				</ul>
-			</div>
+	<!-- packages & pricing -->
+	<div class="pricing-section">
+		<div class="line" />
+		<p class="text-sm my-10">PRICING</p>
+		<div class="packages">
+			{#each PACKAGES as p}
+				<div class="package">
+					<span class="package-header">
+						<h1>{p.title}</h1>
+						<p>●</p>
+						<h2>{p.price}</h2>
+					</span>
+					<ul class="package-services border-pr">
+						{#each p.services as s}
+							<li>{s}</li>
+						{/each}
+					</ul>
+				</div>
+			{/each}
 		</div>
 	</div>
 </section>
@@ -72,7 +54,29 @@
 	.min {
 		min-width: 250px;
 	}
-	.border-price {
-		@apply border border-gray-800 border-opacity-50;
+	.pricing-section {
+		@apply flex flex-col justify-start items-center mt-10 mx-6;
+	}
+	.packages {
+		@apply w-full flex flex-col sm:flex-row max-w-5xl py-8 mb-10 justify-center space-x-0 space-y-3 sm:space-x-4 sm:space-y-0;
+	}
+	.package {
+		@apply w-full mb-6 sm:my-0;
+	}
+	.package-header {
+		@apply flex w-full justify-center items-center mb-10 space-x-2;
+	}
+	.package-header h1 {
+		@apply text-3xl font-black text-center;
+	}
+	.package-header p {
+		@apply text-sm;
+	}
+	.package-header h2 {
+		@apply text-3xl opacity-60 font-light;
+	}
+	.package-services {
+		height: 13rem;
+		@apply text-center space-y-5 border border-gray-800 border-opacity-50 flex flex-col justify-center items-center;
 	}
 </style>
