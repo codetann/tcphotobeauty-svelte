81db7e9b8e376b56d34429a09384d2fd7c889fa7
codetann <thetannerthomas@gmail.com>
2021-12-08 03:48:11 -0700

    update contact page

--- src/lib/notion.js
+++ src/lib/notion.js
@@ -1,8 +1,60 @@
-import { Client } from '@notion/client';
+import { Client } from '@notionhq/client';
+import { v4 as uuidv4 } from 'uuid';
 
 const notion = new Client({ auth: import.meta.env.VITE_NOTION_KEY });
-const notionId = import.meta.env.VITE_NOTION_ID;
+const databaseId = import.meta.env.VITE_NOTION_ID;
 
-async function submitForm({ first, last, email, phone, message }) {
+export async function submitForm({ first, last, email, phone, message }) {
 	// implement async logic here
+	const response = await notion.pages.creeate({
+		parent: { database_id: databaseId },
+		properties: {
+			title: {
+				title: [
+					{
+						text: {
+							context: uuidv4()
+						}
+					}
+				]
+			},
+			Name: {
+				text: [
+					{
+						text: {
+							content: `${first} ${last}`
+						}
+					}
+				]
+			},
+			Email: {
+				email: [
+					{
+						text: {
+							content: email
+						}
+					}
+				]
+			},
+			Phone: {
+				phone: [
+					{
+						text: {
+							content: phone
+						}
+					}
+				]
+			},
+			Message: {
+				text: [
+					{
+						text: {
+							content: message
+						}
+					}
+				]
+			}
+		}
+	});
+	return response;
 }
--- src/routes/contact/index.svelte
+++ src/routes/contact/index.svelte
@@ -1,33 +1,105 @@
 <script>
 	import Page from '$components/Page.svelte';
+	import { submitForm } from '$lib/notion';
+
+	let first = null;
+	let last = null;
+	let email = null;
+	let phone = null;
+	let message = null;
+	let isSubmitted = false;
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		if (isDisabled()) {
+			return;
+		}
+		submitForm({ first, last, email, phone, message });
+		isSubmitted = true;
+	}
+
+	$: isDisabled = () => {
+		if (!first || !last || !email || !phone || !message) {
+			return true;
+		}
+		return false;
+	};
 </script>
 
 <Page>
-	<span class="flex space-y-4 sm:space-y-0 sm:space-x-2 flex-col sm:flex-row w-full ">
-		<span class="flex flex-col w-full">
-			<label for="first">First name</label>
-			<input id="first" class=" border-2 border-black border-opacity-90 h-10" type="text" />
-		</span>
-		<span class="flex flex-col w-full">
-			<label for="last">Last name</label>
-			<input id="last" class=" border-2 border-black border-opacity-90 h-10" type="text" />
-		</span>
-	</span>
+	{#if isSubmitted}
+		<div class="alert alert-success">
+			<p>Thank you for contacting us! We will get back to you as soon as we can!</p>
+		</div>
+	{/if}
 
-	<span class="w-full">
-		<label for="email">Email</label>
-		<input id="email" class=" border-2 border-black border-opacity-90 h-10 w-full" type="text" />
-	</span>
+	{#if !isSubmitted}
+		<form on:submit={handleSubmit} class="max-w-xl px-2 flex flex-col w-full items-end">
+			<span class="flex space-y-0 sm:space-y-0 sm:space-x-2 flex-col sm:flex-row w-full ">
+				<span class="flex flex-col w-full">
+					<label for="first">First name</label>
 
-	<span class="flex flex-col w-full">
-		<label for="about">Tell us about yourselves</label>
-		<textarea id="about" class=" border-2 border-black border-opacity-90 h-40" type="text" />
-	</span>
+					<input
+						id="first"
+						class=" border-2 border-black border-opacity-90 h-10"
+						type="text"
+						bind:value={first}
+					/>
+				</span>
+				<span class="flex flex-col w-full">
+					<label for="last">Last name</label>
+					<input
+						id="last"
+						class=" border-2 border-black border-opacity-90 h-10"
+						type="text"
+						bind:value={last}
+					/>
+				</span>
+			</span>
+
+			<span class="w-full">
+				<label for="phone">Phone number</label>
+				<input
+					id="phone"
+					class=" border-2 border-black border-opacity-90 h-10 w-full"
+					type="tel"
+					bind:value={phone}
+				/>
+			</span>
+
+			<span class="w-full">
+				<label for="email">Email</label>
+				<input
+					id="email"
+					class=" border-2 border-black border-opacity-90 h-10 w-full"
+					type="email"
+					bind:value={email}
+				/>
+			</span>
+
+			<span class="flex flex-col w-full">
+				<label for="about">Tell us about yourself</label>
+				<textarea
+					id="about"
+					class=" border-2 border-black border-opacity-90 h-40"
+					type="text"
+					bind:value={message}
+				/>
+			</span>
+
+			<button disabled={isDisabled()} class="btn rounded-none bg-black text-white">Sumbit</button>
+		</form>
+	{/if}
 </Page>
 
 <style lang="postcss">
 	input {
 		padding-left: 0.5rem;
+		@apply mb-2;
+	}
+	textarea {
+		padding-left: 0.5rem;
+		@apply mb-2;
 	}
 	label {
 		@apply text-gray-800;
