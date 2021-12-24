2a5f83a6524a67b289bfb225844097fa475c08b1
Tanner <thetannerthomas@gmail.com>
2021-12-23 23:14:13 -0700

    create slug page

--- src/routes/photo/[slug].svelte
+++ src/routes/photo/[slug].svelte
@@ -1,5 +1,29 @@
 <script>
+	import PhotoTile from '$components/PhotoTile.svelte';
+	import Clients from '$components/Clients.svelte';
+	import Page from '$components/Page.svelte';
+	import { localstorage } from '$lib/utils';
 	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+
+	let uid = $page.params.slug;
+	let gallery = [];
+
+	onMount(() => {
+		const clients = localstorage.get('clients');
+		if (clients) {
+			const client = clients.filter((client) => client.uid === uid);
+			gallery = client[0].gallery;
+		}
+	});
 </script>
 
-{$page.params.slug}
+<Page>
+	<Clients>
+		{#if gallery}
+			{#each gallery as G}
+				<PhotoTile url={G.url} />
+			{/each}
+		{/if}
+	</Clients>
+</Page>
